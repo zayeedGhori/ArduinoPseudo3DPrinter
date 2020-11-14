6e7d9b36ed5f79827469b527af5084626207644e
Zayeed Ghori <zmh030609@gmail.com>
2020-11-14 18:48:18 -0500

    Fixed test code.

--- Pseudo3DPrinter.ino
+++ Pseudo3DPrinter.ino
@@ -7,13 +7,12 @@ Servo servo1;
 void setup() {
   servo1.attach(servoPin);
 
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
   servo1.write(180);
   delay(3000);
 
   servo.write(90);
   delay(1000);
+}
+
+void loop() {
 }
