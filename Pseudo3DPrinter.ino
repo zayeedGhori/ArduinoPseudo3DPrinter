85b1b5c85ed268b7f7c151d0553a41cb58df8cc6
Zayeed Ghori <zmh030609@gmail.com>
2020-11-14 18:38:16 -0500

    Added servo code to test.

--- Pseudo3DPrinter.ino
+++ Pseudo3DPrinter.ino
@@ -1,9 +1,19 @@
+// Include the Servo library 
+#include <Servo.h>
+
+int servoPin = 3;
+Servo servo1;
+
 void setup() {
-  // put your setup code here, to run once:
+  servo1.attach(servoPin);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  servo1.write(180);
+  delay(3000);
 
+  servo.write(90);
+  delay(1000);
 }
