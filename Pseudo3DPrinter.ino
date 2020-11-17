95e93e8fc3b6f607fc46b8dda6c91e5e50122c2b
Zayeed Ghori <zmh030609@gmail.com>
2020-11-16 22:54:44 -0500

    Added safety stop and test timer for z axis.

--- Pseudo3DPrinter.ino
+++ Pseudo3DPrinter.ino
@@ -2,10 +2,14 @@
 #include <Servo.h>
 
 // servo variables
-int servo1Pin = 3;
+int servoZPin = 3;
 int servo2Pin = 5;
 int servo3Pin = 6;
-Servo servo1, servo2, servo3;
+Servo servoZ, servo2, servo3;
+bool delayRunning = false;
+bool goBackUp = false;
+int delayStart;
+int delayLength = 5000;
 
 // variables for switch
 int switchPinIn = 13;
@@ -14,29 +18,33 @@ int prevState = HIGH;
 
 void setup() {
   // assign servos to pins
-  servo1.attach(servo1Pin);
+  servoZ.attach(servoZPin);
   servo2.attach(servo2Pin);
   servo3.attach(servo3Pin);
-  
+
   pinMode(switchPinIn, INPUT); // make this pin receive voltage
 }
 
 void loop() {
   reading = digitalRead(switchPinIn); // check current state of button
 
-  // if the button is not pressed, make motors run at full speed
-  if (reading == LOW && prevState == HIGH){
-    servo1.write(180);
-    servo2.write(180);
-    servo3.write(180);
+  // if the button is pressed, stop the motors
+  if (reading == HIGH && prevState == LOW) {
+    servoZ.write(90); //stops the motor
+    
+    goBackUp = !goBackUp; // toggles back and forth between true and false
+    delayStart = millis();
   }
 
-  // if the button is pressed, stop the motors
-  else if (reading == HIGH && prevState == LOW){
-    servo1.write(90);
-    servo2.write(90);
-    servo3.write(90);
+  // if the button is not pressed, make motors run at full speed
+  else if (reading == LOW && prevState == HIGH) {
+    if (goBackUp && (millis() - delayStart) <= 1000)
+      servoZ.write(180);
+    
+      
+    else
+      servoZ.write(0);
   }
-  
+
   prevState = reading; // make prevState the current state
 }
