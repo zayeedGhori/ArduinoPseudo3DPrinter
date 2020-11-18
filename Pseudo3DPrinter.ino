427c9badc557892941e1186727fbf0ad19bf0301
Zayeed Ghori <zmh030609@gmail.com>
2020-11-17 21:12:43 -0500

    Added working loop for z-axis.

--- Pseudo3DPrinter.ino
+++ Pseudo3DPrinter.ino
@@ -6,16 +6,17 @@ int servoZPin = 3;
 int servo2Pin = 5;
 int servo3Pin = 6;
 Servo servoZ, servo2, servo3;
-bool delayRunning = false;
-bool goBackUp = false;
-int delayStart;
-int delayLength = 5000;
+
+// variables for servo logic
+bool goDown = true;
+bool goUp = true;
+bool firstTimeGoingUp = true;
 
 // variables for switch
 int switchPinIn = 13;
-int reading;
-int prevState = HIGH;
+bool atLimit;
 
+// run this once
 void setup() {
   // assign servos to pins
   servoZ.attach(servoZPin);
@@ -23,28 +24,48 @@ void setup() {
   servo3.attach(servo3Pin);
 
   pinMode(switchPinIn, INPUT); // make this pin receive voltage
+  Serial.begin(9600); // Prepare serial port to debug if needed
 }
 
+// run this repeatedly
 void loop() {
-  reading = digitalRead(switchPinIn); // check current state of button
+  atLimit = digitalRead(switchPinIn) == LOW; // check current state of button
 
-  // if the button is pressed, stop the motors
-  if (reading == HIGH && prevState == LOW) {
-    servoZ.write(90); //stops the motor
+  // if bot should go up
+  if (goUp){
+    // if it is the first time going up, go up for 2s
+    // else, go up for 25s
+    servoZ.write(180);
     
-    goBackUp = !goBackUp; // toggles back and forth between true and false
-    delayStart = millis();
-  }
-
-  // if the button is not pressed, make motors run at full speed
-  else if (reading == LOW && prevState == HIGH) {
-    if (goBackUp && (millis() - delayStart) <= 1000)
-      servoZ.write(180);
-    
-      
+    if (firstTimeGoingUp)
+      delay(2000);
     else
-      servoZ.write(0);
+      delay(25000);
+
+    firstTimeGoingUp = !firstTimeGoingUp; // change firstTimeGoingUp to the opposite of the curent value
+    goUp = false; // do not go up any more
+    atLimit = true; // force button press to jumpstart going down
   }
 
-  prevState = reading; // make prevState the current state
+  // if button is pressed/limit is reached, 
+  // toggle between going down and going up
+  if (atLimit){
+    if (goDown)
+      servoZ.write(0);
+
+    else{
+      // stop and wait for 2s
+      servoZ.write(90);
+      delay(2000);
+
+      // go up for 2s to unpress the switch
+      servoZ.write(180);
+      delay(2000);
+
+      goUp = true; // z-axis should go up now
+    }
+    
+    goDown = !goDown; // change to opposite of current value
+    delay(200); // delay to make sure a single press is not counted as hold
+  }
 }
